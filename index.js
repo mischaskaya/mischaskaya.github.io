6540d502a1092890f0549edd4edc74dc58beced8
irelia <irelia@macbook.pro>
2024-07-27 08:36:57 -0400

    first

---
+++ index.js
@@ -0,0 +1,8 @@
+//create a ready event
+document.addEventListener('DOMContentLoaded', function() {
+
+    //write a click event handler for the class called "Slide"
+    document.querySelector('.eventhandler').addEventListener('click', function() {
+        alert("hello");
+    });
+});
